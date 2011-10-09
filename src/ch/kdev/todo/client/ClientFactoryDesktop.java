0c4b0c44a900128864e7db8c2d557220cab44bbc
fippiko <fippi.ko@gmail.com>
2011-10-10 00:37:13 +0200

    started with maven

--- src/ch/kdev/todo/client/ClientFactoryDesktop.java
+++
@@ -1,8 +0,0 @@
-package ch.kdev.todo.client;
-
-import com.google.gwt.inject.client.GinModules;
-
-@GinModules(ClientFactoryDesktopModule.class)
-public interface ClientFactoryDesktop extends ClientFactory {
-
-}
