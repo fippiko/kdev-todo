727d933f21a24c6d3921dfc032f4060d772a4b6e
fippiko <fippi.ko@gmail.com>
2011-12-16 23:14:11 +0100

    changed naming of files

--- src/main/java/ch/kdev/todo/client/view/resources/AppStyleDesktopWorkExt.java
+++
@@ -1,8 +0,0 @@
-package ch.kdev.todo.client.view.resources;
-
-
-//this interface is only a workarround, because the generated interface AppStyleDesktop is not public
-public interface AppStyleDesktopWorkExt extends AppStyleDesktop {
-  
-}
-
